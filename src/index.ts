498d5f88e235b29fcfc466b92396d78134dac54d
becks256 <22892728+becks256@users.noreply.github.com>
2024-05-29 00:04:20 -0400

    calls helpers in a consolidated run function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
-import { testFetch } from "./utils"
+import {
+  createConversationsArray,
+  getUserMessages,
+  readFile,
+  saveData,
+  submitConversationsArray,
+  testFile,
+} from "./utils"
 
 const run = async () => {
-  console.log(await testFetch())
+  const DATA_PATH = "src/data"
+  const isDataSaved = await testFile(DATA_PATH)
+
+  let data
+
+  // cache the data to avoid unnecessary API calls during development
+  if (!isDataSaved) {
+    console.log("Data not saved, fetching from API")
+    data = await getUserMessages()
+    await saveData(DATA_PATH, data)
+  }
+
+  data = await readFile(DATA_PATH)
+
+  const conversations = createConversationsArray(data)
+
+  await submitConversationsArray({ conversations })
 }
 
 run()
